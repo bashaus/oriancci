d53e69a43cbda67710a772c7e6cbf500572fc550
Bashkim Isai <bashkim.isai@agencyrepublic.com>
2013-06-24 14:20:01 +0100

    added a pre-commit hook for phpcs to ensure PSR-1 and PSR-2 standards

---
+++ hooks/pre-commit.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+# PHP CodeSniffer pre-commit hook
+
+ROOT=`git rev-parse --show-toplevel`
+
+OUTPUT=$(phpcs --standard=PSR1 --report=full $ROOT/src)
+RETURNED=$?
+
+if [ 0 -ne "$RETURNED" ] ; then
+    echo "There are PSR-1 code style issues."
+    echo "Please correct these before commiting."
+    echo "$OUTPUT"
+    exit $RETURNED
+fi
+
+OUTPUT=$(phpcs --standard=PSR2 --report=full $ROOT/src)
+RETURNED=$?
+
+if [ 0 -ne "$RETURNED" ] ; then
+    echo "There are PSR-2 code style issues."
+    echo "Please correct these before commiting."
+    echo "$OUTPUT"
+    exit $RETURNED
+fi
+
+exit 0
+
--- sniff/sniff.sh
+++
@@ -1,2 +0,0 @@
-phpcs --standard=PSR1 --report-checkstyle=PSR1.xml ../src;
-phpcs --standard=PSR2 --report-checkstyle=PSR2.xml ../src;
