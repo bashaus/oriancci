e41f990d1b3db6c9550780525ee2e97d92c8771d
Bashkim Isai <me@bashkim.com.au>
2013-06-29 09:38:14 +0100

    updated hooks

--- hooks/pre-commit.sh
+++ hooks/pre-commit.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
 ROOT=`git rev-parse --show-toplevel`
-BASE="`dirname \"$0\"`" 
 
 # Check PSR-1 standard
-PSR1_STDOUT=`$BASE/pre-commit/psr-1.sh`
+PSR1_STDOUT=`$ROOT/hooks/pre-commit/psr-1.sh`
 PSR1_RESULT=$?
 echo "$PSR1_STDOUT"
 
 # Check PSR-2 standard
-PSR2_STDOUT=`$BASE/pre-commit/psr-2.sh`
+PSR2_STDOUT=`$ROOT/hooks/pre-commit/psr-2.sh`
 PSR2_RESULT=$?
 echo "$PSR2_STDOUT"
 
 # Check the JSON file
-JSON_STDOUT=`/usr/bin/env php $BASE/pre-commit/composer-json.php`
+JSON_STDOUT=`/usr/bin/env php $ROOT/hooks/pre-commit/composer-json.php`
 JSON_RESULT=$?
 echo "$JSON_STDOUT"
 
