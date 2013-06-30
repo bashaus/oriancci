6bdc9470b23653703ec40e13b046e37adde774a7
Bashkim Isai <me@bashkim.com.au>
2013-06-30 04:58:37 +0100

    added test for databaseName static model method

--- tests/Oriancci/models/department.php
+++ tests/Oriancci/models/department.php
@@ -5,6 +5,11 @@ namespace Oriancci\Models;
 class Department extends \Oriancci\Model\BTree
 {
 
+	public static function databaseName()
+	{
+		return ORIANCCI_PHPUNIT_DATABASE;
+	}
+
     public static function tableName()
     {
         return 'department';
